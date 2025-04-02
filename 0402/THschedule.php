76030224ae1de64958874e5d23be2fb037efa2a7
Xuwei0407 <xzwiii47@mail.com>
2025-04-02 11:41:03 +0800

    0402 unfinish

---
+++ 0402/THschedule.php
@@ -0,0 +1,140 @@
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF8">
+<meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT">
+<meta http-equiv="pragma" content="no-cache">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.6">
+<title>網頁程式設計與安全實務：Project 20250312</title>
+<style type="text/css">
+<!--
+body {font-family: Microsoft JhengHei, arial; font-size: 1em; color: #000000 }
+th {font-family: Microsoft JhengHei, arial; }
+td {font-family: Microsoft JhengHei, arial; }
+select {font-family: Microsoft JhengHei, arial; }
+    -->
+</style>
+</head>
+<body>
+<div style="width:800px;margin:20px auto;text-align:center;">
+<h1>教室授課表: 資工系所 包蒼龍 老師</h1>
+<table width="100%" class="cistab" align="center">
+					<tr>
+            <td width="10%" align="center" bgcolor="#00008b">
+							<font color=White>節　次</font></td>
+						<td width="15%" align="center" bgcolor=#00008b>
+							<font color=Yellow>星期一</font></td>
+						<td width="15%" align="center" bgcolor="#00008b">
+							<font color=Yellow>星期二</font></td>
+						<td width="15%" align="center" bgcolor="#00008b">
+							<font color=Yellow>星期三</font></td>
+						<td width="15%" align="center" bgcolor="#00008b">
+							<font color=Yellow>星期四</font></td>
+						<td width="15%" align="center" bgcolor="#00008b">
+							<font color=Yellow>星期五</font></td>
+						<td width="15%" align="center" bgcolor="#00008b">
+							<font color=Yellow>星期六</font></td>
+					</tr>
+				
+<tr> 
+<td align="center" bgcolor="#00008b"><font color="white">第一節<br>08:10<br>09:00 </font></td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+</tr>
+
+<tr> 
+<td align="center" bgcolor="#00008b"><font color="white">第二節<br>09:10<br>10:00 </font></td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+<td bgcolor="#AACCDD" align="center" rowspan="3">I4010<br><a href="SbjDetail.php?SbjNo=I4010" target="_BLANK">網頁程式</a><br>A3-100<br>
+</td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+<td bgcolor="#AACCDD" align="center" rowspan="3">I6110<br><a href="SbjDetail.php?SbjNo=I6110" target="_BLANK">系統防護</a><br>A3-404<br>
+</td>
+</tr>
+
+<tr> 
+<td align="center" bgcolor="#00008b"><font color="white">第三節<br>10:10<br>11:00 </font></td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+<td bgcolor="#AACCDD" align="center" rowspan="2">G4713<br><a href="SbjDetail.php?SbjNo=G4713" target="_BLANK">連結臺灣(三)</a><br>A8-106<br>
+</td>
+</tr>
+
+<tr>
+<td align="center" bgcolor="#00008b"><font color="white">第四節<br>11:10<br>12:00 </font></td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+</tr>
+
+<tr>
+<td align="center" bgcolor="DarkRed"><font color="white">中　午<br>12:10<br>13:00 </font></td>
+<td bgcolor="paleGreen"><br>&nbsp;</td>
+<td bgcolor="paleGreen"><br>&nbsp;</td>
+<td bgcolor="paleGreen"><br>&nbsp;</td>
+<td bgcolor="paleGreen"><br>&nbsp;</td>
+<td bgcolor="paleGreen"><br>&nbsp;</td>
+<td bgcolor="paleGreen"><br>&nbsp;</td>
+</tr>
+
+<tr>
+<td align="center" bgcolor="#00008b"><font color="white">第五節<br>13:10<br>14:00 </font></td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+<td bgcolor="#AACCDD" align="center" rowspan="2">G0040A<br><a href="SbjDetail.php?SbjNo=G0040A" target="_BLANK">現代公民素養</a><br>A8-B105<br>
+</td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+</tr>
+
+<tr>
+<td align="center" bgcolor="#00008b"><font color="white">第六節<br>14:10<br>15:00 </font></td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+</tr>
+
+<tr> 
+<td align="center" bgcolor="#00008b"><font color="white">第七節<br>15:10<br>16:00 </font></td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+</tr>
+
+<tr>
+<td align="center" bgcolor="#00008b"><font color="white">第八節<br>16:10<br>17:00 </font></td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+<td bgcolor="#AACCDD"><br>&nbsp;</td>
+</tr>
+
+<tr>
+<td align="center" bgcolor="DarkRed"><font color="white">傍　晚<br>17:10<br>18:00 </font></td>
+<td bgcolor="paleGreen" align="center">G0070G<br><a href="SbjDetail.php?SbjNo=G0070G" target="_BLANK">勞作教育</a><br>-<br>
+</td>
+<td bgcolor="paleGreen"><br>&nbsp;</td>
+<td bgcolor="paleGreen"><br>&nbsp;</td>
+<td bgcolor="paleGreen"><br>&nbsp;</td>
+<td bgcolor="paleGreen"><br>&nbsp;</td>
+<td bgcolor="paleGreen"><br>&nbsp;</td>
+</tr>
+				</table>
+</div>
+</body>
+</html>
